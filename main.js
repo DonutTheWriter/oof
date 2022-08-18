c5b9b7638903f0aca17eea420a7ba107ee1ad636
DonutTheWriter <104382042+DonutTheWriter@users.noreply.github.com>
2022-08-18 15:54:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var forts = "Hardik fricking threw water at me.";
+var second= "HE SHOULD DIE";
+var full_idoit= forts.concat (second);
+
+console.log(full_idoit);
+
+var a = 4;
+var b= 8;
+var add= a+b;
+
+console.log(add);
